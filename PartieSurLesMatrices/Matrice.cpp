3d97f4585b6c77f603b4d75e700d9008ae93de96
jixinhe <jessiehe1993@gmail.com>
2017-03-22 20:59:52 +0100

    9h00

--- PartieSurLesMatrices/Matrice.cpp
+++ PartieSurLesMatrices/Matrice.cpp
@@ -39,6 +39,44 @@ template<class MyType>  CMatrice<MyType>::CMatrice(CMatrice &vCmat)
 	}
 }
 
+template<class MyType>  unsigned int CMatrice<MyType>::MATLireNbLigne()
+{
+	// retourner la valeur de la ligne
+	return uiMATNbLigne;
+}
+
+template<class MyType>  unsigned int CMatrice<MyType>::MATLireNbColonne()
+{
+	// retourner la valeur de la colonne
+	return uiMATNbColonne;
+}
+
+template<class MyType>  void CMatrice<MyType>::MATCreerMatrice(unsigned int ligne, unsigned int colonne)
+{
+	// jeter une exception
+	if (ligne <= 0 || colonne <= 0){
+		Cexception exc1("Error: illegal matrix size.");
+		throw exc1;
+	}
+	// definir la ligne et colonne
+	uiMATNbLigne = ligne;
+	uiMATNbColonne = colonne;
+
+	//allouer la memoire
+	if (pMATElement == NULL){
+		Cexception exc2("Error: fail to malloc.");
+		throw exc2;
+	}
+	pMATElement = new MyType*[uiMATNbLigne];
+	for (i = 0; i < uiMATNbColonne; i++){
+		if (pMATElement[i] == NULL){
+			Cexception exc3("Error: fail to malloc.");
+			throw exc3;
+		}
+		pMATElement[i] = new MyType[uiMATNbColonne];
+	}
+}
+
 template<class MyType> 	void  CMatrice<MyType>::MATConcretiserMatrice(MyType *pLigne, unsigned int ligne)
 {
 	int j = 0;
@@ -51,6 +89,24 @@ template<class MyType> 	void  CMatrice<MyType>::MATConcretiserMatrice(MyType *pL
 	}
 }
 
+template<class MyType>  CMatrice<MyType> & CMatrice<MyType>::MATTransposer()
+{
+	unsigned int ligneT = uiMATNbColonne;
+	unsigned int colonneT = uiMATNbLigne;
+	CMatrice<MyType> *matriceT = new CMatrice<MyType>;
+
+	// creer la matrice transposee
+	matriceT->MATCreerMatrice(ligneT, colonneT);
+	for (i = 0; i < ligneT; i++)
+	{
+		for (j = 0; j < colonneT; j++)
+		{
+			matriceT->pMATElement[i][j] = pMATElement[j][i];
+		}
+	}
+	// retourner la matrice transposee
+	return *matriceT;
+}
 
 template<class MyType> CMatrice<MyType> &  CMatrice<MyType>::operator*(double dPara)
 {
@@ -67,3 +123,33 @@ template<class MyType> CMatrice<MyType> &  CMatrice<MyType>::operator*(double dP
 }
 
 template<class MyType> CMatrice<MyType> &  CMatrice<MyType>::operator+(double dPara)
+
+
+template<class MyType> CMatrice<MyType> & CMatrice<MyType> :: operator*(CMatrice<MyType> mPara)
+{
+	MyType somme;
+	CMatrice<MyType> *matriceProduit = new CMatrice<MyType>;
+	// jeter une exception
+	if (uiMATNbColonne != mPara.uiMATNbLigne)
+	{
+		Cexception exc1("Error: illegal matrix size for multiplication.");
+		throw exc1;
+	}
+
+	// multiplication
+	matriceProduit->MATCreerMatrice(uiMATNbLigne, mPara.uiMATNbColonne);
+	for (i = 0; i < uiMATNbLigne; i++)
+	{
+		for (j = 0; j < mPara.uiMATNbColonne; j++)
+		{
+			somme = 0;
+			for (k = 0; k < uiMATNbColonne; k++)
+			{
+				somme = somme + pMATElement[i][k] * mPara.pMATElement[k][j];
+			}
+			matriceProduit[i][j]
+
+		}
+	}
+
+}
